7b06c17861597810ed95a36e04ebef4b365f67fb
Praveen Muthukumarana <praveenmuth2@gmail.com>
2020-07-16 11:51:19 +0530

    installed mongoose

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
 
 const schoolRouter = require('./routes/schoolRouter');
 
@@ -18,6 +19,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/', schoolRouter);
+
+// connect to database
+mongoose.connect('mongodb://localhost/schools', { useNewUrlParser: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected');
+});
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
