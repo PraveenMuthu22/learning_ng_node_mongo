0fffae02dce22b3a8c37571ab838602655628937
Praveen Muthukumarana <praveenmuth2@gmail.com>
2020-08-01 14:10:53 +0530

    observeon

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { Observer, Subject, from, Observable, of, interval, fromEvent, Subscription, throwError } from 'rxjs';
-import { filter, map, timeout, catchError, take, multicast, refCount, publish, share, publishBehavior, publishLast, publishReplay } from 'rxjs/operators';
+import { of, queueScheduler, asapScheduler, asyncScheduler, merge, from } from 'rxjs';
+import { observeOn, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -11,33 +10,18 @@ import { filter, map, timeout, catchError, take, multicast, refCount, publish, s
 export class AppComponent {
   title = 'angular-app';
 
-  constructor(private router: Router) { }
-
   ngOnInit(): void {
-    this.router.navigate(['/schools']);
 
-    const source$ = interval(1000).pipe(
-      take(4),
-      publishReplay(),
-      refCount()
-    );
+    console.log('Start script');
 
-    source$.subscribe(value => console.log(`Observer 1: ${value}`));
+    from([1, 2, 3, 4], queueScheduler)
+    .pipe(
+      tap(value => console.log(`Value: ${value}`)),
+      observeOn(asyncScheduler),
+      tap(value => console.log(`Value Doubled: ${value * 2}`))
+    ).subscribe();
 
-    setTimeout(() => {
-      source$.subscribe(value => console.log(`Observer 2: ${value}`));
-    }, 1000);
 
-    setTimeout(() => {
-      source$.subscribe(value => console.log(`Observer 3: ${value}`));
-    }, 3000);
-
-    setTimeout(() => {
-      source$.subscribe(
-        value => console.log(`Observer 4: ${value}`),
-        error => null,
-        () => console.log('Observer 4 completed')
-        );
-    }, 4500);
+    console.log('End script');
   }
 }
